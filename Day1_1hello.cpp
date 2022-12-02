61576deee22aebd0a6c04450af41baa6799b6c44
AdeelaShahid <92438784+AdeelaShahid@users.noreply.github.com>
2022-12-03 00:57:05 +0530

    Add files via upload

---
+++ Day1_1hello.cpp
@@ -0,0 +1,8 @@
+//  First C++ Program
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello People!";
+    return 0;
+}
---
+++ Day1_2Addition.cpp
@@ -0,0 +1,12 @@
+//Addition
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,sum;
+	sum=0;
+	cin>>a>>b;
+	cout<<"adding "<<a<<" and "<<b<<endl;
+	sum=a+b;
+	cout<<sum;
+	return 0;
+}
