d40762040e4d3965ea5736bf2d0ea0b0423dc0f7
AdeelaShahid <92438784+AdeelaShahid@users.noreply.github.com>
2022-12-09 22:03:24 +0530

    Add files via upload

---
+++ Day8-16th-array.cpp
@@ -0,0 +1,16 @@
+//array
+#include<iostream>
+using namespace std;
+int main(){
+    int i,a[10];
+    cout<<"enter the elements in array"<<endl;
+    for(i=0; i<=5; i++){
+    cin>>a[i];
+    }
+    //int n[50] = {10,20,30,40};
+    cout<<"array"<<endl;
+    for(i=0; i<=5; i++){
+
+    cout<<a[i]<<endl;
+    }
+}
